17175e0ef0be5697ffd4191e321cf0cf3f784310
pontakornth <pontakorn61@gmail.com>
2023-04-20 20:37:54 +0700

    Use S3 storage setting

--- backend_service/settings.py
+++ backend_service/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-msktvjilb(n&-3vd1(ic6)7rvy_%6qd6-a%2%q_q)(m21lad9@
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend_service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -81,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -101,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -112,7 +108,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -127,3 +122,28 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/movies_service/media/'
+
+# Storage
+USE_S3_ON_DEBUG = config("S3_DEBUG", default=False, cast=bool)
+
+if DEBUG and USE_S3_ON_DEBUG:
+    default_storage = "storages.backends.s3boto3.S3Boto3Storage"
+elif DEBUG:
+    default_storage = "django.core.files.storage.FileSystemStorage"
+else:
+    default_storage = "storages.backends.s3boto3.S3Boto3Storage"
+
+
+STORAGES = {"default": {"BACKEND": default_storage}}
+
+# AWS
+# Please note that when you use EC2 please attach to IAM role instead
+# - Pontakorn Paesaeng
+
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID") if DEBUG else ""
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY") if DEBUG else ""
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+
+# If it is True, it will expose access key to the public.
+# Please do not do that.
+AWS_QUERYSTRING_AUTH=False
